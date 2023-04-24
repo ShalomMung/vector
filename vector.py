bb76f4efd2a58b8a6287876b4ababb4b55affeb5
ShalomMung <shallomung@outlook.com>
2023-04-24 12:35:32 +0200

    Added docstring to __add__

--- vector.py
+++ vector.py
@@ -12,6 +12,7 @@ class Vector3D:
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
 
     def __add__(self, other):
+        """Add two vectors"""
         print("self = ", self)
         print("other =", other)
         w = Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
