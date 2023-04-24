9098292fa4119527d27f0f131bd9782abca40b30
ShalomMung <shallomung@outlook.com>
2023-04-24 11:17:06 +0200

    test_vector.py

--- test_vector.py
+++ test_vector.py
@@ -1,2 +1,5 @@
-form vector import Vector3D
+from vector import Vector3D
 
+def test_vector_str():
+    v = Vector3D(1, 2, 3)
+    assert str(v) == '(1, 2, 3)'
--- vector.py
+++ vector.py
@@ -1,7 +1,13 @@
 
 class Vector3D:
-    def __init__(self)(self, x, y, z):
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y 
         self.z = z 
-        
+    
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+
+if __name__ == "__main__":
+    v = Vector3D(1, 2, 3)
+    print(v)
