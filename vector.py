e0a8d2ead6576e206a65c6014cdd02176e880677
ShalomMung <shallomung@outlook.com>
2023-04-25 11:18:55 +0200

    modified vector.py

--- vector.py
+++ vector.py
@@ -1,10 +1,21 @@
-
 class Vector3D:
+    """
+    A call for 3D vector
+    
+    Arguments
+    ---------
+    x : float
+        First number to be added
+    y : float
+        Second number to be added
+    z : float
+        Third number to be added
+    """
     def __init__(self, x, y, z):
         self.x = x
-        self.y = y 
-        self.z = z 
-    
+        self.y = y
+        self.z = z
+
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
 
@@ -29,6 +40,7 @@ class Vector3D:
 
     def dot(self, other):
         return self.x * other.x + self.y * other.y + self.z * other.z
+
 
 if __name__ == "__main__":
     v = Vector3D(1, 2, 3)
